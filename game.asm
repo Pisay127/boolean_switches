4a8492abea29b58121c669fad5ff7dfed7b5e134
Sean Francis N. Ballais <sean@seanballais.com>
2018-01-28 19:33:34 +0800

    Finished the simulation.

--- game.asm
+++ game.asm
@@ -114,8 +114,10 @@ handle_keypresses:
     cmp al, 50 ; '2'
     je handle_OR_switch
     cmp al, 51 ; '3'
-    je handle_first_not_color_switch
+    je handle_XOR_switch
     cmp al, 52 ; '4'
+    je handle_first_not_color_switch
+    cmp al, 53 ; '5'
     je handle_second_not_color_switch
     jne check_mouse_input
 
@@ -125,6 +127,10 @@ handle_keypresses:
 
     handle_OR_switch:
         mov andor_text_flag, 1
+        jmp check_mouse_input
+
+    handle_XOR_switch:
+        mov andor_text_flag, 2
         jmp check_mouse_input
 
     handle_first_not_color_switch:
@@ -168,13 +174,16 @@ draw:
     ; Draw "wires"
     mov gx_color, 42
     ; Draw first input "wire"
-    draw_rect 24, 48, 161, 50
+    draw_rect 74, 48, 86, 50
+    draw_rect 111, 48, 159, 50
     ; Draw second input "wire"
-    draw_rect 24, 123, 161, 125
+    draw_rect 74, 123, 86, 125
+    draw_rect 111, 123, 159, 125
     ; Draw "wire" through the AND/OR switch
-    draw_rect 159, 48, 161, 125
+    draw_rect 159, 48, 161, 61
+    draw_rect 159, 111, 161, 125
     ; Draw "wire" from the AND/OR switch to the OUTPUT
-    draw_rect 159, 85, 260, 87
+    draw_rect 185, 85, 210, 87
 
     ; Draw the first input
     mov_byte_vars gx_color, first_input_color
@@ -196,6 +205,8 @@ draw:
     mov_byte_vars gx_color, andor_color
     draw_rect andor_x, andor_y, andor_x1, andor_y1
 
+    call set_output_color
+
     mov_byte_vars gx_color, output_color
     draw_rect output_x, output_y, output_x1, output_y1
 
@@ -203,23 +214,39 @@ draw:
     mov gx_color, 15
     ; Draw first vertical line
     draw_rect 147, 73, 149, 98
-    ; Draw second vertical line
-    draw_rect 172, 73, 174, 98
-    ; Draw first horizontal line
-    draw_rect 147, 73, 172, 75
 
     cmp andor_text_flag, 0
-    je draw_A
-    jne draw_B ; We can just move to b since there are only two ops (AND, OR)
+    je draw_AND
+    cmp andor_text_flag, 1
+    je draw_OR
+    jne draw_XOR
 
-    draw_A:
+    draw_AND:
+        ; Draw second vertical line
+        draw_rect 172, 73, 174, 98
+        ; Draw first horizontal line
+        draw_rect 147, 73, 172, 75
         ; Draw second horizontal line
         draw_rect 147, 84, 172, 86
 
         jmp draw_return_point
-    draw_B:
+    draw_OR:
+        ; Draw second vertical line
+        draw_rect 172, 73, 174, 98
+        ; Draw first horizontal line
+        draw_rect 147, 73, 172, 75
         ; Draw second horizontal line
         draw_rect 147, 96, 172, 98
+
+        jmp draw_return_point
+
+    draw_XOR:
+        ; Draw first horizontal line
+        draw_rect 147, 73, 172, 75
+        ; Draw second horizontal line
+        draw_rect 147, 96, 172, 98
+        ; Draw middle horizontal line
+        draw_rect 147, 85, 172, 87
 
         jmp draw_return_point
 
@@ -272,5 +299,109 @@ handle_right_click:
     set_second_input_off:
         mov second_input_color, 18
         jmp change_input_color_return
+
+set_output_color:
+    xor dx, dx
+
+    cmp first_input_color, 18 ; Off
+    je  set_first_input_bit_off
+    jne set_first_input_bit_on
+
+    set_first_input_bit_off:
+        mov dh, 0
+        jmp first_input_return
+
+    set_first_input_bit_on:
+        mov dh, 1
+
+        jmp first_input_return
+
+    first_input_return:
+
+    cmp first_not_input_color, 1 ; Off
+    je  first_not_input_return
+    jne set_first_input_flip
+
+    set_first_input_flip:
+        ; I have to do this hack because using `not dh` doesn't seem to work. Weird!
+        cmp dh, 0
+        je set_dh_1
+        jne set_dh_0
+
+        set_dh_1:
+            mov dh, 1
+            jmp first_not_input_return
+
+        set_dh_0:
+            mov dh, 0
+            jmp first_not_input_return
+
+    first_not_input_return:
+
+    cmp second_input_color, 18 ; Off
+    je  set_second_input_bit_off
+    jne set_second_input_bit_on
+
+    set_second_input_bit_off:
+        mov dl, 0
+        jmp second_input_return
+
+    set_second_input_bit_on:
+        mov dl, 1
+
+        jmp second_input_return
+
+    second_input_return:
+
+    cmp second_not_input_color, 1 ; Off
+    je  second_not_input_return
+    jne set_second_input_flip
+
+    set_second_input_flip:
+        ; I have to do this hack because using `not dh` doesn't seem to work. Weird!
+        cmp dl, 0
+        je set_dl_1
+        jne set_dl_0
+
+        set_dl_1:
+            mov dl, 1
+            jmp second_not_input_return
+
+        set_dl_0:
+            mov dl, 0
+            jmp second_not_input_return
+
+    second_not_input_return:
+
+    cmp andor_text_flag, 0 ; AND is set
+    je  set_output_AND
+    cmp andor_text_flag, 1 ; OR is set
+    je  set_output_OR
+    jne set_output_XOR
+
+    set_output_AND:
+        and dh, dl
+        jmp output_color_return
+
+    set_output_OR:
+        or dh, dl
+        jmp output_color_return
+
+    set_output_XOR:
+        xor dh, dl
+        jmp output_color_return
+
+    output_color_return:
+
+    cmp dh, 0 ; Output is off
+    je  set_output_off
+    jne set_output_on
+
+    set_output_off:
+        mov output_color, 18
+        ret
+    set_output_on:
+        mov output_color, 14
+        ret
 
 end start
